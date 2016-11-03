e67a717724c162bd2a3864787bf759cbf02f691f
YDQ <614552310@qq.com>
2016-11-03 22:07:38 +0800

    Update KLeastNumbers.cpp

--- KLeastNumbers.cpp
+++ KLeastNumbers.cpp
@@ -66,3 +66,64 @@ void main()
 
 }
 
+//2016_11_3
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int partition(vector<int>& A,int l,int r)
+{
+    int pivot = A[l];
+    while(l<r)
+    {
+        while(l<r&&A[l]<=pivot)
+        {
+            l++;
+        }
+        swap(A[l],A[r]);
+        while(l<r&&A[r]>=pivot)
+        {
+            r--;
+        }
+        swap(A[r],A[l]);
+    }
+    return l;
+}
+
+int KLeastNumber(vector<int>& nums,int k)
+{
+    if(k>nums.size()||k<=0||nums.size()<0)
+    {
+        return 0;
+    }
+    int l =0;
+    int r = nums.size()-1;
+    int index = partition(nums,l,r);
+    while(index!=k-1)
+    {
+        while(index<k-1)
+        {
+            l = index+1;
+            index=partition(nums,l,r);
+        }
+        while(index>k-1)
+        {
+            r = index-1;
+            index = partition(nums,l,r);
+          
+        }
+    }
+    for(int i =0 ;i<index;i++)
+    {
+    	printf("%d",nums[i]);
+    }
+    return 0;
+}
+int main()
+{
+	vector<int> nums(7);
+	nums[0] = 2;
+	nums[1] = 4;
+	nums[2] = 3;
+	nums[3]	= 7;
+
